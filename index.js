4e1f1c614825b4eea3de5121e412dba0dd95b1f0
Tuukka Virtanen <duukkis@gmail.com>
2021-02-03 02:02:57 +0200

    starting the actual assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log('Hello world')
+const express = require( 'express' )
+const app = express()
+
+const baseUrl = 'https://bad-api-assignment.reaktor.com/v2'
+const glovesUrl = `${baseUrl}/products/gloves`
+
+app.get( glovesUrl, ( req, res ) => {
+    console.log( res.data )
+})
+
+const PORT = process.env.PORT || 3001
+app.listen( PORT, () => {
+  console.log( `Server running on port ${PORT}` )
+})
