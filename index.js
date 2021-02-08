abdd861bb6bece9bca1227fcdd18c2018acb70ea
Tuukka Virtanen <duukkis@gmail.com>
2021-02-08 23:51:59 +0200

    only express.static( 'build' )

--- index.js
+++ index.js
@@ -1,14 +1,10 @@
 const express = require( 'express' )
 const app = express()
-
-const baseUrl = 'https://bad-api-assignment.reaktor.com/v2'
-const glovesUrl = `${baseUrl}/products/gloves`
-
-app.get( glovesUrl, ( req, res ) => {
-    console.log( res.data )
-})
+const cors = require( 'cors' )
+app.use( cors() )
+app.use( express.static( 'build' ))
 
 const PORT = process.env.PORT || 3001
 app.listen( PORT, () => {
-  console.log( `Server running on port ${PORT}` )
+	console.log( `Server running on port ${PORT}` )
 })
