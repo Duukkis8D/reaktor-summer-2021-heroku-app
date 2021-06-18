c7ca78ec4c7326663dd67ba5cc8948f79320a901
Tuukka Virtanen <duukkis@gmail.com>
2021-06-19 00:19:05 +0300

    tried another type of retry functionality in product availability requests but again, this would not work on Heroku due to H12 timeout error

--- index.js
+++ index.js
@@ -9,6 +9,66 @@ app.use( ( req, res, next ) => {
 	next()
 } )
 
+const createProductDataPromise = ( baseUrl, productCategory, res ) => {
+	axios
+		.get( `${baseUrl}/products/${productCategory}` )
+		.then( response => {
+			console.log(
+				'response.config.url in server response:', response.config.url, '\n',
+				'response.status in server response:', response.status
+			)
+
+			res.send( response.data )
+		} ).catch( error => {
+			console.error( 'error occurred while handling product data (name, color, price etc):', error )
+		} )
+}
+
+const createProductAvailabilityPromise = ( baseUrl, productManufacturer, res ) => {
+	axios
+		.get( `${baseUrl}/availability/${productManufacturer}` )
+		.then( serverResponse => {
+			const productAvailabilityData = serverResponse.data.response
+
+			console.log(
+				'serverResponse.config.url in server response:', serverResponse.config.url, '\n',
+				'serverResponse.status in server response:', serverResponse.status
+			)
+
+			if( productAvailabilityData.length > 2 ) {
+				res.send( productAvailabilityData )
+			} else if( productAvailabilityData.length <= 2 ) {
+				console.error( 
+					'invalid server response detected with url:', serverResponse.config.url, '\n',
+					'productAvailabilityData.length:', productAvailabilityData.length, '\n',
+					'requesting data again...'
+				)
+
+				createProductAvailabilityPromise( baseUrl, productManufacturer, res )
+			}
+		} ).catch( error => {
+			console.error( 'error occurred while handling product availability data:', error )
+		} )
+}
+
+// axios response interceptor for logging responses.
+/*axios.interceptors.response.use( response => {
+	console.log(
+		'response.config.url in server response:', response.config.url, '\n',
+		'response.status in server response:', response.status
+	)
+
+	if( response.config.url.includes( 'availability' ) ) {
+		if( response.data.response.length <= 2 ) {
+			console.error( 
+				'invalid server response detected with url:', response.config.url, '\n',
+				'response.data.response.length:', response.data.response.length, '\n',
+				'requesting data again...' 
+			)
+		}
+	}
+} )*/
+
 /* Forwards the frontend request to Reaktor Bad API server and the response back to frontend.
 GET /v2/products/:category – Return a listing of products in a given category: gloves, facemasks or beanies.
 GET /v2/availability/:manufacturer – Return a list of availability info.
@@ -20,64 +80,22 @@ app.get( '/api', ( req, res ) => {
 	// Reaktor Bad API URL
 	const baseUrl = 'https://bad-api-assignment.reaktor.com/v2'
 
-	// axios response interceptor. Request is sent again if the array of server response is empty.
-	axios.interceptors.response.use( response => {
-		console.log(
-			'response.config.url in server response:', response.config.url,
-			"response.headers['content-type'] in server response:", response.headers['content-type']
-		)
-
-		if( response.config.url.includes( 'availability' ) ) {
-			if( response.data.response.length <= 2 ) {
-				console.error( 
-					'invalid server response detected with url:', response.config.url,
-					'response.data.response.length:', response.data.response.length,
-					'requesting data again...' 
-				)
-	
-				return axios.get( response.config.url )
-			}
-		}
-
-		return response
-	}, error => {
-		return Promise.reject( 'error occurred during API request:', error )
-	} )
-
 	// craft full URL, make request to Bad API and forward response
 	if ( category !== undefined ) {
 		console.log( 
-			`requesting product data (name, color, price etc) with the following product 
-			category query parameter:`, category 
+			'product category query parameter for requesting product data (name, color, price etc):', 
+			category 
 		)
 
-		axios
-			.get( `${baseUrl}/products/${category}` )
-			.then( response => {
-				res.send( response.data )
-			} ).catch( error => {
-				console.error( 
-					'error occurred while handling product data (name, color, price etc):', 
-					error 
-				)
-			} )
+		createProductDataPromise( baseUrl, category, res )
 	}
 	else if ( manufacturer !== undefined ) {
 		console.log( 
-			`requesting product availability data with the following product manufacturer 
-			query parameter:`, manufacturer 
+			'product manufacturer query parameter for requesting product availability data:', 
+			manufacturer
 		)
 
-		axios
-			.get( `${baseUrl}/availability/${manufacturer}` )
-			.then( response => {
-				res.send( response.data.response )
-			} ).catch( error => {
-				console.error( 
-					'error occurred while handling product availability data:', 
-					error 
-				)
-			} )
+		createProductAvailabilityPromise( baseUrl, manufacturer, res )
 	}
 	else {
 		console.error( 'unidentified query parameter (neither product category nor manufacturer)' )
